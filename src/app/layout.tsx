89ed4d7258aabed0d284d57dd4b51ba9c297f39d
eliveltonsf <elivelton.seven@gmail.com>
2024-03-07 12:49:58 -0300

    feat: add theme next and shadcn ui configuration for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import '@/styles/globals.css';
 
 import localFont from 'next/font/local';
 
+import {cn} from '@/lib/utils'
+import Header from '@/components/Header';
+
 const wotfard = localFont({
   src: [
     {
@@ -25,7 +28,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={wotfard.className}><AppProvider>{children}</AppProvider></body>
+      <body className={cn(wotfard.className, "mx-[8rem]")}>
+        <AppProvider>
+          <Header />
+          {children}
+        </AppProvider>
+      </body>
     </html>
   )
 }
