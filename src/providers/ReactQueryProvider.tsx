861fc611e93af2e76e956776db371d1e68493c38
eliveltonsf <elivelton.seven@gmail.com>
2024-03-07 12:53:25 -0300

    feat: update provider with themeProvider

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ThemeProvider } from "@/components/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
@@ -9,5 +10,17 @@ interface QueryProviderProps {
 export function QueryProvider({ children }: QueryProviderProps) {
   const client = new QueryClient();
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  const teste = "teste";
+
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {" "}
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
+    </ThemeProvider>
+  );
 }
