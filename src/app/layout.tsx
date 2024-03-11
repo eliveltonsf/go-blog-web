1670326c860e868be44f4d113367bf0b1e74f57d
eliveltonsf <elivelton.seven@gmail.com>
2024-03-11 16:43:22 -0300

    refactor: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,42 @@
-import { AppProvider } from '@/providers';
-import '@/styles/globals.css';
+import { AppProvider } from "@/providers";
+import "@/styles/globals.css";
 
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 
-import {cn} from '@/lib/utils'
-import Header from '@/components/Header';
+import { cn } from "@/lib/utils";
+import Header from "@/components/Header";
+import Footer from "./footer";
 
 const wotfard = localFont({
   src: [
     {
-      path: '../fonts/wotfard-regular-webfont.woff2',
-      weight: '400',
-      style: 'regular'
+      path: "../fonts/wotfard-regular-webfont.woff2",
+      weight: "400",
+      style: "regular",
     },
-  ]
+  ],
 });
 
 export const metadata = {
-  title: 'Go Blog',
-  description: 'Go Blog',
-}
+  title: "Go Blog",
+  description: "Go Blog",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={cn(wotfard.className, "mx-[8rem]")}>
+      <body
+        className={cn(wotfard.className, "overflow-x-hidden style-scrollbar-h")}
+      >
         <AppProvider>
           <Header />
           {children}
         </AppProvider>
       </body>
     </html>
-  )
+  );
 }
