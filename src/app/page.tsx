5b1603d6f35b5069c4e13a58715a77f7d20c5c38
eliveltonsf <elivelton.seven@gmail.com>
2024-03-11 16:43:57 -0300

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Banner from "@/components/banner";
+import Best from "@/components/best";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import Recent from "@/components/recent";
 
 export default function Home() {
   return (
-    <main className="flex items-center">
-      <p className="text-16">Elivelton Ferreira</p>
+    <main className="flex flex-col items-center px-24 sm:px-[11.2rem]">
+      <Hero />
+      <Recent />
+      <Best />
+      <Banner />
+      <Footer />
     </main>
   );
 }
