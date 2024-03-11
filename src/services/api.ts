00ad15e6945afa28c6d564bf079ff52c5c0471a0
eliveltonsf <elivelton.seven@gmail.com>
2024-03-11 17:01:42 -0300

    fix: update url service

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ type getNewsDataProps = {
 
 export const getNewsData = async ({query}:getNewsDataProps) => {
   const response = await axios.get(
-   `http://servicodados.ibge.gov.br/api/v3/noticias${query && query}`,
+   `https://servicodados.ibge.gov.br/api/v3/noticias${query && query}`,
    {
     headers: {
       'Content-Type': 'application/json',
