641e17a098d74a1b6a9477134827615e01018c34
eliveltonsf <elivelton.seven@gmail.com>
2024-03-11 16:41:37 -0300

    feat: add footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <section className="flex justify-between items-center h-[8rem] w-full mt-[40rem] border-t border-t-primary">
+      <div className="flex justify-start items-center w-[50%]">
+        <p>2024 | Todos os direitos reservados.</p>
+      </div>
+      <div className="flex justify-end items-center w-[50%]">
+        social
+        {/* <img src={facebook} className="icon-link icon-s" alt="" />
+          <img src={instagram} className="icon-link icon-s ml-2" alt="" />
+          <img src={youtube} className="icon-link icon-s ml-2" alt="" />
+          <img src={twitter} className="icon-link icon-s ml-2" alt="" /> */}
+      </div>
+    </section>
+  );
+}
